6b1531d24f013a8ac783415ef447dd47d8feee4c
avishek jha <avishekjha@avisheks-Laptop.local>
2025-03-05 15:48:59 +1100

    app for testing

---
+++ WorkerNode/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const hugeData = require("./large-file.json");
+
+app.use(express.json());
+
+app.get("/simple", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/hugeData", (req, res) => {
+  res.send(hugeData);
+});
+
+app.get("/slow", (req, res) => {
+  setTimeout(() => {
+    res.send("hello again");
+  }, 3000);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
